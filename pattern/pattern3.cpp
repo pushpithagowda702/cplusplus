c4324cc01647eceb94dc1a023929d62b57fe84a5
pushpitha <pushpithagowda776@gmail.com>
2023-07-16 14:03:37 +0530

    Inverted Pyramid

---
+++ pattern/pattern3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int rows;
+  cout<<"Enter number of rows: ";
+  cin>>rows;
+  for(int i=0; i<rows; i++) {
+    for(int j=0; j<i; j++) {
+      cout<<" ";
+    }
+    for(int j=i; j<rows; j++) {
+      cout<<"* ";
+    }
+    cout<<endl;
+  }
+  return 0;
+}
